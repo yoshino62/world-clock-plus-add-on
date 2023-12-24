328f4aa1b2cd87bb6237fb8e1c8aa1efa9bb4084
yoshino62 <150293831+yoshino62@users.noreply.github.com>
2023-12-23 22:21:06 -0500

    Added a link to all cities

--- javascript/index.js
+++ javascript/index.js
@@ -69,7 +69,8 @@ function updateCity(event) {
                 <div class="time">${cityTime.format(
                   "h:mm:ss"
                 )} <small>${cityTime.format("A")}</small></div>
-            </div>`;
+            </div>
+            <a href="/"> All Cities </a>`;
 }
 
 let selectedCityElement = document.querySelector("#city");
